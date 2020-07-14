336251ddf0adad13efbdd6e5e05e9c5d9068ca95
Emmanuel Jose <emjose@gmail.com>
2020-07-14 22:18:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,7 @@
 # Add your variables here
-first_number = 10
-second_number = 20
+first_number = 25
+second_number = 5
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first number / second_number
